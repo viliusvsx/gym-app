3d383b7f6c73abb56af733847d23ef2275bdccf6
Vilius Vlasovas <vilius.vlasovas@gmail.com>
2025-12-11 07:20:21 +0200

    Add habit reminder schedule to console routes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Jobs\SendHabitReminder;
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function (): void {
+    User::query()
+        ->whereHas('habits', fn ($query) => $query->where('reminder_enabled', true))
+        ->each(fn (User $user) => SendHabitReminder::dispatch($user->id));
+})
+    ->name('habit-reminders')
+    ->dailyAt('07:00');
